bf75f6ff9213931866db441c85eca7cbd512946c
Roger Collado <87712273+R-Collado@users.noreply.github.com>
2023-03-28 18:43:29 +0200

    Add files via upload

--- src/app/main/agency-spotlight/agency-spotlight.component.ts
+++ src/app/main/agency-spotlight/agency-spotlight.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-agency-spotlight',
@@ -10,6 +12,17 @@ export class AgencySpotlightComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+    this.playVideo();
+  }
+
+  playVideo(): void {
+    const video = document.querySelector('.video') as HTMLVideoElement;
+    video.muted = true;
+    video.play();
+    video.onended = e => {
+      video.play();
+    }
   }
 
 }

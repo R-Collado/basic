080f4ebe43ffc60b9dc849141d56ece94d38ed98
Roger Collado <roger.collado1130@gmail.com>
2023-04-19 13:05:46 +0200

    add  menu images and text

--- src/app/shared/travels-menu/travels-menu.component.ts
+++ src/app/shared/travels-menu/travels-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-travels-menu',
@@ -10,43 +11,43 @@ export class TravelsMenuComponent implements OnInit {
   travels = [
     {
       idx: '01',
-      name: 'b/d jams',
+      name: 'Kyoto',
       year: 2022,
-      catchphrase: "it's a vibe",
-      img: 'https://images.pexels.com/photos/23735/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      summary: 'A weekly-ish playlist bla bla blabla'
+      catchphrase: "おはよう!",
+      img: 'https://images.unsplash.com/photo-1545569341-9eb8b30979d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      summary: 'A must-see destination for anyone traveling to Japan. With its rich history, stunning temples and gardens, and unique cultural experiences, Kyoto is a true gem of a city. '
     },
     {
       idx: '02',
-      name: 'b/d jams',
+      name: "Uyuni's Salt Lake",
       year: 2022,
-      catchphrase: "it's a vibe",
-      img: 'https://images.pexels.com/photos/23735/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      summary: 'A weekly-ish playlist bla bla blabla'
+      catchphrase: "Where the sky meets the earth",
+      img: 'https://images.unsplash.com/photo-1583901370237-2befec865f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      summary: "Also known as Salar de Uyuni, is the world's largest salt flat located in the Potosí and Oruro departments in southwest Bolivia."
     },
     {
       idx: '03',
-      name: 'b/d jams',
+      name: 'Banff',
       year: 2022,
-      catchphrase: "it's a vibe",
-      img: 'https://images.pexels.com/photos/23735/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      summary: 'A weekly-ish playlist bla bla blabla'
+      catchphrase: "Find your mountain bliss",
+      img: 'https://images.pexels.com/photos/2695391/pexels-photo-2695391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      summary: "Rich with wildlife and showcasing panoramic views of mountains and lakes, Banff is a spectacular destination for those that love the great outdoors."
     },
     {
       idx: '04',
-      name: 'b/d jams',
+      name: 'Lake Bohinj, at Slovenia',
       year: 2022,
-      catchphrase: "it's a vibe",
-      img: 'https://images.pexels.com/photos/23735/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      summary: 'A weekly-ish playlist bla bla blabla'
+      catchphrase: "Escape to paradise",
+      img: 'https://images.pexels.com/photos/11289836/pexels-photo-11289836.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      summary: "It is a nature lover's paradise. Visitors can explore the gorgeous environs by hiking, biking, and kayaking."
     },
     {
       idx: '05',
-      name: 'b/d jams',
+      name: 'Faroe Islands',
       year: 2022,
-      catchphrase: "it's a vibe",
-      img: 'https://images.pexels.com/photos/23735/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      summary: 'A weekly-ish playlist bla bla blabla'
+      catchphrase: "Uncover the untouched beauty",
+      img: 'https://images.pexels.com/photos/2581853/pexels-photo-2581853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      summary: "While this Danish archipelago is known for its tiny, colorful houses, it's also rich in dynamic scenery, from basalt cliffs to waterfalls."
     }
   ];
 
@@ -69,7 +70,6 @@ export class TravelsMenuComponent implements OnInit {
 
        travelList.onmousemove = e => {
         const movement = e.clientX;
-        const rect =  travelList.getBoundingClientRect();
 
         if(!this.listenMouse) return;
         
@@ -77,11 +77,11 @@ export class TravelsMenuComponent implements OnInit {
         else translate--
 
         if (translate > 0 ) translate = 0;
-        else if (translate < -75) translate = -75;
+        else if (translate < -100) translate = -100;
 
         gsap.to(travelList, {
           xPercent: translate,
-          duration: .5
+          duration: 2
         });
 
         actualX = e.clientX;

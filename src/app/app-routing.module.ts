351e201a4c8f710d36dcd9f2e090c6e8d39fe493
Roger Collado <roger.collado1130@gmail.com>
2023-04-20 00:43:53 +0200

    add news component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full' },
-  { path: 'about', component: AboutComponent}
+  { path: 'about', component: AboutComponent },
+  { path: 'news', component: NewsComponent }
 ];
 
 @NgModule({

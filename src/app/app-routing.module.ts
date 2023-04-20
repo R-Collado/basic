a0b89a451337402a90db365d97debef9267f2716
Roger Collado <roger.collado1130@gmail.com>
2023-04-20 15:54:05 +0200

    add ideas component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { NewsComponent } from './news/news.component';
+import { IdeasComponent } from './ideas/ideas.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: 'news', component: NewsComponent }
+  { path: 'news', component: NewsComponent },
+  { path: 'ideas', component: IdeasComponent}
 ];
 
 @NgModule({

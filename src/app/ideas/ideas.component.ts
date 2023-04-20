da3ac35974ef2fa60cb635c553ff4824bc2258e9
Roger Collado <roger.collado1130@gmail.com>
2023-04-20 15:54:27 +0200

    create ideas component and module

---
+++ src/app/ideas/ideas.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NavMenuService } from '../shared/nav-menu/nav-menu.service';
+import { gsap } from 'gsap';
+
+@Component({
+  selector: 'app-ideas',
+  templateUrl: './ideas.component.html',
+  styleUrls: ['./ideas.component.scss']
+})
+export class IdeasComponent implements OnInit {
+
+  constructor(public navService: NavMenuService) { }
+
+  ngOnInit(): void {
+    const aboutComponent = document.querySelector('#ideas-component') as HTMLElement;
+
+    const oldColors = this.navService.toggleRootColors('dark', false);
+    
+    gsap.fromTo(aboutComponent, {
+      background: oldColors?.bg,
+      color: oldColors?.fg
+    }, {
+      background: '#252422',
+      color: '#f9cdcd',
+      duration: 1 
+    })
+  }
+
+}

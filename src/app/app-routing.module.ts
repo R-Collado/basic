d2c663ca2739fa856a8fd8ebc8ce212029aa0d54
Roger Collado <roger.collado1130@gmail.com>
2023-04-27 12:45:35 +0200

    add contact route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { NewsComponent } from './news/news.component';
 import { IdeasComponent } from './ideas/ideas.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'news', component: NewsComponent },
-  { path: 'ideas', component: IdeasComponent}
+  { path: 'ideas', component: IdeasComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({

2ff98df3ba3ac0a4bad18fdaa0f69235c847ebbc
Roger Collado <roger.collado1130@gmail.com>
2023-04-28 14:11:25 +0200

    initial commit contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavMenuService } from '../shared/nav-menu/nav-menu.service';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(public navService: NavMenuService) { }
 
   ngOnInit(): void {
+    const newsComponent = document.querySelector('#contact-component') as HTMLElement;
+    const imageContainer = document.querySelector('.image-container') as HTMLElement;
+    const heading = document.querySelector('h2') as HTMLElement;
+
+    setTimeout(() => {
+      const oldColors = this.navService.toggleRootColors('light', false);
+      let easing = gsap.parseEase(".5, 0, 0, 1");
+
+
+      gsap.fromTo(newsComponent, {
+        background: oldColors?.bg,
+        color: oldColors?.fg
+      }, {
+        background: '#f4f4f4',
+        color: '#252422',
+        duration: 1 
+      });
+
+      gsap.from(heading, { 
+        yPercent: 103,
+        stagger: 0.05,
+        duration: .5,
+        ease: easing
+      });
+    })
   }
 
 }
